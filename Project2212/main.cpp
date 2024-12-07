2fa469f67f7efbf3821a817ea99a6b2efa305ac9
Natasha <natasasandro9@gmail.com>
2024-12-07 11:13:30 +0700

    мой первый действительный комментарий

---
+++ Project2212/main.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Russian"); // переключение локализации консоли
+	unsigned int new_n, n;
+	cout << "Введите n: ";  cin >> n;
+	new_n = 1 << n;// Сдвигаем число 1 влево на n бит, что эквивалентно вычислению 2^n и сохраняем результат в new_n
+	cout << new_n << "(10)" << endl;
+	cout << hex << new_n << "(16)" << endl;
+	cout << oct << new_n << "(8)";
+}
